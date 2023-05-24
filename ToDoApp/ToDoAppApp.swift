0f615720f000127cc7d83c3a781ae8e14ebfc6f2
Prashant Kaware <prashant@Prashants-MacBook-Pro.local>
2023-05-24 18:54:57 +0530

    commit message

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Prashant Kaware on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
